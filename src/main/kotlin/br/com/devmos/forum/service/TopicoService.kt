a993d17979cf3358e1fd29ecf776e90ae94f0011
Milton <miltonrubro@gmail.com>
2022-05-26 21:50:44 -0300

    Alterando criação de lista de topicos

--- src/main/kotlin/br/com/devmos/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/devmos/forum/service/TopicoService.kt
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
+class TopicoService(private var topicos: List<Topico>) {
 
+    init {
         val topico = Topico(
             id = 1,
             titulo = "Duvida Kotlin",
@@ -25,6 +25,26 @@ class TopicoService {
                 email = "ana@email.com"
             )
         )
-        return Arrays.asList(topico, topico, topico)
+
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin 2",
+            mensagem = "Variaveis no Kotlin 2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"
+            ),
+            usuario = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+        topicos =  Arrays.asList(topico, topico2)
+
+    }
+    fun listar(): List<Topico> {
+        return topicos
     }
 }
