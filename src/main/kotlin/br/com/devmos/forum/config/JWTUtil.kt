c5e56687ae76153ce569294ce3b6c9b1a755512d
Milton <miltonrubro@gmail.com>
2022-08-09 21:25:49 -0300

    correções

--- src/main/kotlin/br/com/devmos/forum/config/JWTUtil.kt
+++ src/main/kotlin/br/com/devmos/forum/config/JWTUtil.kt
@@ -8,39 +8,39 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Component
-import java.lang.IllegalArgumentException
 import java.util.*
 
 @Component
 class JWTUtil(
     private val usuarioService: UsuarioService
 ) {
+
     private val expiration: Long = 60000
 
     @Value("\${jwt.secret}")
     private lateinit var secret: String
 
-    fun generateToken(userName: String, authorities: MutableCollection<out GrantedAuthority>): String?{
+    fun generateToken(username: String, authorities: MutableCollection<out GrantedAuthority>): String? {
         return Jwts.builder()
-                    .setSubject(userName)
-                    .claim("rol", authorities)
-                    .setExpiration(Date(System.currentTimeMillis() + expiration))
-                    .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
-                    .compact()
+            .setSubject(username)
+            .claim("role", authorities)
+            .setExpiration(Date(System.currentTimeMillis() + expiration))
+            .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
+            .compact()
     }
 
     fun isValid(jwt: String?): Boolean {
-        return try{
+        return try {
             Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt)
             true
-        }catch (e : IllegalArgumentException){
+        } catch (e: IllegalArgumentException) {
             false
         }
     }
 
-    fun getAuthentication(jwt: String?): Authentication {
-        val userName = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt).body.subject
-        val user = usuarioService.loadUserByUsername(userName)
-        return UsernamePasswordAuthenticationToken(userName, null, user.authorities)
+    fun getAuthentication(jwt: String?) : Authentication {
+        val username = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt).body.subject
+        val user = usuarioService.loadUserByUsername(username)
+        return UsernamePasswordAuthenticationToken(username, null, user.authorities)
     }
 }
